73a8f276f776a000fc2f3e89680c6bbc9d19a27d
JackMasterson <jackmasterson5@gmail.com>
2016-06-06 19:04:48 -0400

    Change top half with JS instead of HTML

--- js/script.js
+++ js/script.js
@@ -16,3 +16,26 @@ function about() {
 		$('.about').show();
 	});
 };
+
+var atfView = {
+	
+	topHalf: function() {
+		var landing = "<div class='full-width'></div>";
+		var shade = "<div class='shade header'></div>";
+		var heading = "<h1 class='head-header'>Cooking by Jack</h1>";
+		var subHeading = "<h2 class='head-sub-header'>Meals for Any Occasion</h2>";
+		
+		$('body').prepend(landing);
+		$('.full-width').append(shade);
+		$('.header').append(heading);
+		$('.header').append(subHeading);
+
+	},
+
+	bottomHalf: function() {
+
+	}
+
+};
+
+atfView.topHalf();
